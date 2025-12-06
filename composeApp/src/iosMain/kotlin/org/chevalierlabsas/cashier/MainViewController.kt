82b74b6a347446b0b5161a11c6faa495d0b86c8e
zazazaxky-m <athallahzacky0204@gmail.com>
2025-12-06 18:51:59 +0700

    first commit

---
+++ composeApp/src/iosMain/kotlin/org/chevalierlabsas/cashier/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.chevalierlabsas.cashier
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ composeApp/src/jvmMain/kotlin/org/chevalierlabsas/cashier/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package org.chevalierlabsas.cashier
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
